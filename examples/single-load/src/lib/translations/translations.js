3fd0a89663f1e6427cf4c52a02172e5d7d29bb03
Jayk <20700514+jayk09@users.noreply.github.com>
2021-12-22 17:18:24 +0100

    Lint

--- examples/single-load/src/lib/translations/translations.js
+++ examples/single-load/src/lib/translations/translations.js
@@ -4,7 +4,7 @@ export default ({
   en: {
     lang,
     menu: {
-      notification: 'You have {{count:gt; 0:{{count}} new {{count; 1:message; default:messages}}!; default:no messages...}}'
+      notification: 'You have {{count:gt; 0:{{count}} new {{count; 1:message; default:messages}}!; default:no messages...}}',
     },
     content: {
       title: 'Welcome to SvelteKit',
@@ -15,12 +15,12 @@ export default ({
   cs: {
     lang,
     menu: {
-      notification: '{{count:gt; 0:Máte {{count}} {{count:gte; 1:novou zprávu; 2:nové zprávy; 5:nových zpráv}}!; default:Nemáte žádné zprávy...}}'
+      notification: '{{count:gt; 0:Máte {{count}} {{count:gte; 1:novou zprávu; 2:nové zprávy; 5:nových zpráv}}!; default:Nemáte žádné zprávy...}}',
     },
     content: {
       title: 'Vítejte ve SvelteKit',
       info: 'Nyní máte přístup ke všem jazykovým mutacím prostřednictvím <code>{$l(LOCALE, TRANSLATION_KEY)}</code>:',
-      text: 'Dokumentace je k přečtení na <a href="{{link}}">kit.svelte.dev</a>'
-    }
+      text: 'Dokumentace je k přečtení na <a href="{{link}}">kit.svelte.dev</a>',
+    },
   },
-})
+});
