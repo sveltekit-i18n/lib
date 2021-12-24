0830c2f5e55d71a97d82e887ab7c288c632625f6
Jayk <20700514+jayk09@users.noreply.github.com>
2021-12-24 23:48:56 +0100

    Update vars

--- examples/custom-modifiers/src/routes/index.svelte
+++ examples/custom-modifiers/src/routes/index.svelte
@@ -2,12 +2,12 @@
   import { t } from '$lib/translations'; 
   import { now } from 'svelte/internal';
 
-  const link = 'https://kit.svelte.dev';
-  const time = (10 * 60 * 1000);
+  const time = Date.now() - (10 * 60 * 1000);
+  const currency = 100;
 </script>
 
 <h1>{$t('content.title_built-in')}</h1>
-<p>{$t('content.modifier_date', { value: Date.now() - time})}</p>
-<p>{$t('content.modifier_ago', { value: Date.now() - time})}</p>
+<p>{$t('content.modifier_date', { value: time})}</p>
+<p>{$t('content.modifier_ago', { value: time})}</p>
 <h1>{$t('content.title_custom')}</h1>
-<p>{$t('content.modifier_currency', { value: 100})}</p>
+<p>{$t('content.modifier_currency', { value: currency})}</p>
