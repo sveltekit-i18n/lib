eb812e77f81cc69d8c8cf790df00617d4fd4abe4
Jayk <20700514+jayk09@users.noreply.github.com>
2021-12-25 22:45:55 +0100

    Edit findOption

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -2,4 +2,4 @@ import type { ModifierOption } from '../types';
 
 export const useDefault = <T = any>(value: any, def:any = {}): T => value || def;
 
-export const findOption = <T = string>(options: ModifierOption[], key: string, defaultValue: string): T => ((options.find((option) => option.key === key))?.value || defaultValue) as any;
+export const findOption = <T = string>(options: ModifierOption[], key: string, defaultValue?: string): T => ((options.find((option) => option.key === key))?.value || defaultValue) as any;
