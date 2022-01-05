9792458788d07f96a85296338e8082accecb6e21
Jayk <20700514+jayk09@users.noreply.github.com>
2022-01-05 22:26:06 +0100

    Rename `Loading` type

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,6 @@
 import { Readable } from 'svelte/store';
 
-export type Loading = Readable<boolean> & { toPromise: () => Promise<void[]> };
+export type LoadingStore = Readable<boolean> & { toPromise: () => Promise<void[]> };
 
 export type Loader = () => Promise<Record<any, any>> | Record<any, any>;
 
