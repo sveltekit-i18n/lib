d114b3aa49d15da8ea610b7a26cf7bcd8356c017
Jayk <20700514+jayk09@users.noreply.github.com>
2022-02-07 01:15:23 +0100

    Fix watch

--- tsup.config.js
+++ tsup.config.js
@@ -7,7 +7,7 @@ export default defineConfig((options) => {
     dts: true,
     format: ['cjs', 'esm'],
     entry: ['src/index.ts'],
-    watch: ['src/*', 'node_modules/@sveltekit-i18n/*'],
+    watch: options.watch && ['src/*', 'node_modules/@sveltekit-i18n/*'],
     minify: !options.watch,
     sourcemap: options.watch,
   };
