543b3c5888dd51d755df7b7b56cc14ba9b77152b
Jayk <20700514+jayk09@users.noreply.github.com>
2022-04-06 23:00:34 +0200

    Update `parser-icu` dependencies

--- examples/parser-icu/svelte.config.js
+++ examples/parser-icu/svelte.config.js
@@ -1,13 +1,17 @@
 /* eslint-disable */
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
   preprocess: preprocess(),
+
   kit: {
-    adapter: adapter(),
-  },
+    adapter: adapter()
+  }
 };
 
 export default config;
+
