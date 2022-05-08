55f9815584d9d494b4c570411dcb919dcb507360
Jayk <20700514+jayk09@users.noreply.github.com>
2022-05-08 18:24:51 +0200

    Replace loaders by translations

---
+++ examples/locale-router-static/src/lib/translations/en/index.js
@@ -0,0 +1,11 @@
+import about from './about.json';
+import error from './error.json';
+import home from './home.json';
+import menu from './menu.json';
+
+export default {
+  about,
+  error,
+  home,
+  menu,
+};
