79d32c99613bb6306e4a30d1f9e204f808f21e0f
jarda-svoboda <20700514+jarda-svoboda@users.noreply.github.com>
2022-05-18 09:55:00 +0000

    Fix inferring types from config

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,22 @@
 import Base from '@sveltekit-i18n/base';
 import parser from '@sveltekit-i18n/parser-default';
 
-import type { Parser } from '@sveltekit-i18n/parser-default';
+import type { Parser, Modifier } from '@sveltekit-i18n/parser-default';
 import type { Config } from './types';
 
 export type { Config };
 
-const normalizeConfig = ({ parserOptions = {}, ...rest }: Config) => ({
+const normalizeConfig = <P = Parser.PayloadDefault, M = Modifier.DefaultProps>({ parserOptions = {}, ...rest }: Config<P, M>) => ({
   ...rest,
   parser: parser(parserOptions),
 });
 
-class I18n<Params extends Parser.Params> extends Base<Params> {
-  constructor(config?: Config) {
+class I18n<Params extends Parser.Params<P, M>, P = Parser.PayloadDefault, M = Modifier.DefaultProps> extends Base<Params> {
+  constructor(config?: Config<P, M>) {
     super(config && normalizeConfig(config));
   }
 
-  loadConfig = (config: Config) => super.configLoader(normalizeConfig(config));
+  loadConfig = (config: Config<P, M>) => super.configLoader(normalizeConfig<P, M>(config));
 }
 
 export default I18n;
