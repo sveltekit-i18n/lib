ed932f9888ebac8edcd698f510a904962b6d542b
Jayk <20700514+jayk09@users.noreply.github.com>
2022-07-30 16:52:39 +0200

    Fix `parser-icu` build

--- examples/parser-icu/vite.config.js
+++ examples/parser-icu/vite.config.js
@@ -3,6 +3,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
+  ssr: {
+    noExternal: true, // or ['@sveltekit-i18n/*', 'intl-messageformat', '@formatjs/*']
+  }
 };
 
 export default config;
