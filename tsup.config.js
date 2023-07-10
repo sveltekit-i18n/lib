44a83aefa45ddaaeeb22c8687c70b62fb187febd
Jayk <20700514+jayk09@users.noreply.github.com>
2023-07-10 02:12:10 +0200

    Fix duplicit declaration on build

--- tsup.config.js
+++ tsup.config.js
@@ -2,13 +2,24 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig(
   /** @type {() => import('tsup').Options} */
-  (options) => ({
-    clean: true,
-    dts: true,
-    format: ['cjs', 'esm'],
-    entry: ['src/index.ts'],
-    minify: !options.watch,
-    sourcemap: options.watch,
-    splitting: true,
-  }),
+  (options) => [
+    {
+      clean: true,
+      dts: true,
+      format: ['esm'],
+      entry: ['src/index.ts'],
+      minify: !options.watch,
+      sourcemap: options.watch,
+      splitting: true,
+    },
+    {
+      clean: false,
+      dts: false,
+      format: ['cjs'],
+      entry: ['src/index.ts'],
+      minify: !options.watch,
+      sourcemap: options.watch,
+      splitting: true,
+    },
+  ],
 );
